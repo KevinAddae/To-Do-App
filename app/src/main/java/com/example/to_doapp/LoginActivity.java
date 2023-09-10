5467c052d40b65ca725def9961dbe2d3f8ef2433
KevinAddae <kevinaddae94@gmail.com>
2023-09-10 19:45:26 +0100

    Added comments to the top of LoginActivity, explaining its use

--- app/src/main/java/com/example/to_doapp/LoginActivity.java
+++ app/src/main/java/com/example/to_doapp/LoginActivity.java
@@ -3,13 +3,31 @@ package com.example.to_doapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * This Class is the and its connected xml file is the launching screen. This class aims to take the user information inputted and check
+ * The user information using firebase auth. If the information is correct the user will be sent to the main menu activity.
+ *  Otherwise, on the occasion the user does not have an account the user can be sent to the activity that is responsible for creating accounts.
+ *
+ */
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView email,pass;
+    private Button loginBtn, registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); // This hides the title bar
+
+
+
+
 
     }
 }
