7c64193237dee97921263af93f961719006eb0c2
KevinAddae <kevinaddae94@gmail.com>
2023-09-10 17:33:26 +0100

    Completed user class

--- app/src/main/java/com/example/to_doapp/Model/User.java
+++ app/src/main/java/com/example/to_doapp/Model/User.java
@@ -1,4 +1,37 @@
 package com.example.to_doapp.Model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String id;
+    private String username;
+
+    public User (String id, String username) {
+        this.id = id;
+        this.username = username;
+
+    }
+    public User() {
+        id = "" ;
+        username = "";
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
 }
