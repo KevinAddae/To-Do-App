2ac8ba740c6928805e9a4c8bf0a97effd6b9e4f4
KevinAddae <kevinaddae94@gmail.com>
2023-09-10 20:09:33 +0100

    Added on part of validation

--- app/src/main/java/com/example/to_doapp/LoginActivity.java
+++ app/src/main/java/com/example/to_doapp/LoginActivity.java
@@ -3,20 +3,29 @@ package com.example.to_doapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This Class is the and its connected xml file is the launching screen. This class aims to take the user information inputted and check
  * The user information using firebase auth. If the information is correct the user will be sent to the main menu activity.
- *  Otherwise, on the occasion the user does not have an account the user can be sent to the activity that is responsible for creating accounts.
+ * Otherwise, on the occasion the user does not have an account the user can be sent to the activity that is responsible for creating accounts.
  *
  */
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView email,pass;
+    private EditText email,pass;
     private Button loginBtn, registerBtn;
+    private CheckBox rememberMe;
+    private TextView forgetMe;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +34,15 @@ public class LoginActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide(); // This hides the title bar
 
+        email = findViewById(R.id.edit_email);
+        pass = findViewById(R.id.edit_password);
 
+        loginBtn = findViewById(R.id.btn_login);
 
-
-
+        loginBtn.setOnClickListener(v -> {
+            if (email.getText().toString().isEmpty() || pass.getText().toString().isEmpty())
+                Toast.makeText(LoginActivity.this, "Please Complete All Fields", Toast.LENGTH_LONG).show();
+        });
     }
+
 }
