ff040535fbd73099f195ddddfbf3f0b470313a4b
KevinAddae <kevinaddae94@gmail.com>
2023-09-11 20:05:55 +0100

    Firebase auth login to check for successful login is added

--- app/src/main/java/com/example/to_doapp/LoginActivity.java
+++ app/src/main/java/com/example/to_doapp/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.to_doapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -10,6 +12,11 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * This Class is the and its connected xml file is the launching screen. This class aims to take the user information inputted and check
@@ -24,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     private CheckBox rememberMe;
     private TextView forgetMe;
 
-
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +41,33 @@ public class LoginActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide(); // This hides the title bar
 
+        auth = FirebaseAuth.getInstance();
+
         email = findViewById(R.id.edit_email);
         pass = findViewById(R.id.edit_password);
 
         loginBtn = findViewById(R.id.btn_login);
+
 /**
  * Create Firebase project for this, connect accounts the continue user validation.
  */
         loginBtn.setOnClickListener(v -> {
             if (email.getText().toString().isEmpty() || pass.getText().toString().isEmpty())
                 Toast.makeText(LoginActivity.this, "Please Complete All Fields", Toast.LENGTH_LONG).show();
+            else {
+                auth.signInWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i);
+                        finish();
+                    } else
+                        Toast.makeText(this, "Incorrect Password or Email, Please try again", Toast.LENGTH_SHORT).show();
+                });
+            }
         });
+
+        reg
     }
 
 }
