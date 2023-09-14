fd17e1a61efd4762621939b4c3548a2989b5d140
KevinAddae <kevinaddae94@gmail.com>
2023-09-14 21:19:08 +0100

    Completed the auth part of the activity

--- app/src/main/java/com/example/to_doapp/RegisterActivity.java
+++ app/src/main/java/com/example/to_doapp/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.to_doapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.to_doapp.Model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -39,6 +44,30 @@ public class RegisterActivity extends AppCompatActivity {
             Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
-            finish();        });
+            finish();
+        });
+
+        registerBtn.setOnClickListener(v -> {
+            String txtEmail = email.getText().toString().trim();
+            String txtUsername = username.getText().toString().trim();
+            String txtPassword = password.getText().toString().trim();
+
+            if (txtEmail.isEmpty() || txtPassword.isEmpty() || txtUsername.isEmpty()) {
+                Toast.makeText(this, "Ensure all fields are complete", Toast.LENGTH_SHORT).show();
+            } else if (txtPassword.length() < 6)
+                Toast.makeText(this, "The password length is too short", Toast.LENGTH_SHORT).show();
+            else
+                auth.createUserWithEmailAndPassword(txtEmail,txtPassword).addOnCompleteListener(task -> {
+                   if(task.isSuccessful()){
+                        sendUserInfo();
+                       Toast.makeText(this, "Auth success", Toast.LENGTH_SHORT).show();
+                   }
+                   else
+                       Toast.makeText(this, "Auth Failed", Toast.LENGTH_SHORT).show();
+                });
+        });
+    }
+
+    private void sendUserInfo() {
     }
 }
