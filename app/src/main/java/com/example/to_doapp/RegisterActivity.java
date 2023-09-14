be0dd3109da1b0f7fc8771cce9a6b50846f41d5b
KevinAddae <kevinaddae94@gmail.com>
2023-09-14 18:31:11 +0100

    Initialise Firebase Firestore

--- app/src/main/java/com/example/to_doapp/RegisterActivity.java
+++ app/src/main/java/com/example/to_doapp/RegisterActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.to_doapp.Model.User;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        db = FirebaseFirestore.getInstance();
     }
 }
