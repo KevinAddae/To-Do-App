7cf4a638983b56eaa4b279445de5216ed0988926
KevinAddae <kevinaddae94@gmail.com>
2023-09-15 19:49:02 +0100

    Added id and onCLick on img view

--- app/src/main/java/com/example/to_doapp/MainMenu.java
+++ app/src/main/java/com/example/to_doapp/MainMenu.java
@@ -2,14 +2,33 @@ package com.example.to_doapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainMenu extends AppCompatActivity {
 
+    private ImageView addList;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); // This hides the title bar
+
+        addList = findViewById(R.id.img_addList);
+
+        addList.setOnClickListener(v -> {
+            Intent i = new Intent(MainMenu.this, CreateListActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+            finish();
+        });
     }
 }
