4cd01cc2d1a14aef6f95b552a94eceb5fad50906
KevinAddae <kevinaddae94@gmail.com>
2023-09-15 20:00:15 +0100

    added lines that remove top bar

--- app/src/main/java/com/example/to_doapp/CreateListActivity.java
+++ app/src/main/java/com/example/to_doapp/CreateListActivity.java
@@ -3,6 +3,7 @@ package com.example.to_doapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class CreateListActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class CreateListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); // This hides the title bar
+
+
     }
 }
