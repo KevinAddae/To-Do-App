6baf82a2d3b3953ecc51dc9c0f5726e8ca3a225b
KevinAddae <kevinaddae94@gmail.com>
2023-09-16 19:43:38 +0100

    Added the onClickListener to return back to main menu

--- app/src/main/java/com/example/to_doapp/CreateListActivity.java
+++ app/src/main/java/com/example/to_doapp/CreateListActivity.java
@@ -2,11 +2,19 @@ package com.example.to_doapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class CreateListActivity extends AppCompatActivity {
 
+    private EditText title;
+    private Button addItemBtn;
+    private ImageView arrowBack, addList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +23,18 @@ public class CreateListActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide(); // This hides the title bar
 
+        title = findViewById(R.id.edit_title);
+        addItemBtn = findViewById(R.id.btn_addItem);
+
+        arrowBack = findViewById(R.id.img_backArrow);
+        addList = findViewById(R.id.img_addList);
+
+        arrowBack.setOnClickListener(v -> {
+            Intent i = new Intent(CreateListActivity.this, MainMenu.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+            finish();
+        });
 
     }
 }
