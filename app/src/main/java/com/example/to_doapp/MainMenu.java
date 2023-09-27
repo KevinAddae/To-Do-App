f589d14f70b888ebbb10558993baae5175fbe680
KevinAddae <kevinaddae94@gmail.com>
2023-09-27 21:19:47 +0100

    db gets lists and shows on recycler

--- app/src/main/java/com/example/to_doapp/MainMenu.java
+++ app/src/main/java/com/example/to_doapp/MainMenu.java
@@ -1,5 +1,6 @@
 package com.example.to_doapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -14,8 +16,11 @@ import android.widget.Toast;
 import com.example.to_doapp.Adapter.ParentViewListAdapter;
 import com.example.to_doapp.Model.TodoList;
 import com.example.to_doapp.Model.TodoItem;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -26,6 +31,7 @@ public class MainMenu extends AppCompatActivity {
     private ArrayList<TodoList> todoLists;
     private FirebaseFirestore db;
     private ArrayList<TodoItem> tasks;
+    private ArrayList<String> titles;
 
     @SuppressLint({"MissingInflatedId", "SuspiciousIndentation"})
     @Override
@@ -41,41 +47,40 @@ public class MainMenu extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
         todoLists = new ArrayList<>();
         tasks = new ArrayList<>();
+        titles = new ArrayList<>();
 
+        db.collection("taskLists").get().addOnCompleteListener(task1 -> {
+            for(QueryDocumentSnapshot document: task1.getResult()){
+                titles.add(document.get("title").toString());
+                Log.e("MainMenu",titles.get(0) + "");
+            }
+            titles.stream().distinct().forEach(title -> db.collection("taskLists").whereEqualTo("title",title).get().addOnCompleteListener(task -> {
+                String id = "";
+                for (QueryDocumentSnapshot document: task.getResult()){
+                    if (task.isSuccessful())
+                        tasks.add(new TodoItem(document.get("task").toString(),Boolean.parseBoolean(document.get("completedTask").toString())));
+                    id = document.get("userId").toString();
+                }
+                Log.e("MainMenu", "" + tasks.size());
+                todoLists.add(new TodoList(title, tasks, id));
+                tasks = new ArrayList<>();
+
+                TodoList a = new TodoList();
+                a.setTitle("Test");
+                todoLists.add(a);
+                Toast.makeText(this, todoLists.size() + "", Toast.LENGTH_SHORT).show();
+                ParentViewListAdapter adapter = new ParentViewListAdapter(MainMenu.this,todoLists);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainMenu.this,LinearLayoutManager.HORIZONTAL,false));
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }));
+
+        });
         addList.setOnClickListener(v -> {
             Intent i = new Intent(MainMenu.this, CreateListActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(i);
             finish();
-
-
-        });
-
-        db.collection("taskLists").get().addOnCompleteListener(task -> {
-            String dbTitle = "";
-            String id = "";
-            for (QueryDocumentSnapshot document : task.getResult()) {
-                //if (!dbTitle.equals(document.get("title").toString()) || !dbTitle.isEmpty()) {
-                    //todoLists.add(new TodoList(dbTitle,tasks))
-                    todoLists.add(new TodoList(document.get("title").toString(), tasks, document.get("userId").toString()));
-                    //tasks.clear();
-                //}
-                tasks.add(new TodoItem(document.get("task").toString(),Boolean.parseBoolean(document.get("completedTask").toString())));
-                dbTitle = document.get("title").toString();
-                id = document.get("userId").toString();
-
-            }
-            if (todoLists.size() == 0)
-                todoLists.add(new TodoList(dbTitle, tasks, id));
-
-            TodoList a = new TodoList();
-            a.setTitle("Test");
-            todoLists.add(a);
-            Toast.makeText(this, todoLists.size() + "", Toast.LENGTH_SHORT).show();
-            ParentViewListAdapter adapter = new ParentViewListAdapter(MainMenu.this,todoLists);
-            recyclerView.setLayoutManager(new LinearLayoutManager(MainMenu.this,LinearLayoutManager.VERTICAL,false));
-            recyclerView.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
         });
 
     }
