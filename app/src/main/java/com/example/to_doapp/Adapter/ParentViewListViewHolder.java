b209fc7ddd1a6d009027e7b0c59c35a08cfb1fb5
KevinAddae <kevinaddae94@gmail.com>
2023-09-28 18:20:36 +0100

    Added to container as a variable in the view holder

--- app/src/main/java/com/example/to_doapp/Adapter/ParentViewListViewHolder.java
+++ app/src/main/java/com/example/to_doapp/Adapter/ParentViewListViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.to_doapp.Adapter;
 
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,10 +12,12 @@ import com.example.to_doapp.R;
 public class ParentViewListViewHolder extends RecyclerView.ViewHolder {
     RecyclerView rv_child;
     TextView title;
+    RelativeLayout container;
 
     public ParentViewListViewHolder(@NonNull View itemView) {
         super(itemView);
         title = itemView.findViewById(R.id.rv_title);
         rv_child = itemView.findViewById(R.id.childRecyclerview);
+        container = itemView.findViewById(R.id.Main_container);
     }
 }
