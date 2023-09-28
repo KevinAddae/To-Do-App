f09b5bb4b0fbde09b8941fe0987aadafe8f35aa4
KevinAddae <kevinaddae94@gmail.com>
2023-09-28 18:19:34 +0100

    created to interface for onClick listeners

---
+++ app/src/main/java/com/example/to_doapp/Adapter/SelectListener.java
@@ -0,0 +1,10 @@
+package com.example.to_doapp.Adapter;
+
+import com.example.to_doapp.Model.TodoList;
+
+import java.util.ArrayList;
+
+public interface SelectListener {
+
+    void onItemClicked(TodoList list);
+}
