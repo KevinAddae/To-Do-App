a52e4290402b62214427bc52b7641f94b88d0df0
KevinAddae <kevinaddae94@gmail.com>
2023-10-02 21:37:34 +0100

    Changed from string to TodoItem

--- app/src/main/java/com/example/to_doapp/Adapter/SelectItemListener.java
+++ app/src/main/java/com/example/to_doapp/Adapter/SelectItemListener.java
@@ -1,5 +1,7 @@
 package com.example.to_doapp.Adapter;
 
+import com.example.to_doapp.Model.TodoItem;
+
 public interface SelectItemListener {
-    void onItemClicked(String item);
+    void onItemClicked(TodoItem item);
 }
