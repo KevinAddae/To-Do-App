ef9f54922d5f1918d6762237e1c8cc57e230eb8b
KevinAddae <kevinaddae94@gmail.com>
2023-10-07 21:00:00 +0100

    Created dialog class, implements Onclick

---
+++ app/src/main/java/com/example/to_doapp/CustomDialog.java
@@ -0,0 +1,72 @@
+package com.example.to_doapp;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class CustomDialog extends AppCompatDialogFragment {
+
+    private EditText updateTask;
+    private DialogListener listener;
+    private CheckBox taskStatus;
+    @SuppressLint("MissingInflatedId")
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_popup,null);
+
+        updateTask = view.findViewById(R.id.edit_UpdateTask);
+        taskStatus = view.findViewById(R.id.cb_taskStatus);
+
+
+        builder.setView(view)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = updateTask.getText().toString();
+                        Boolean status = taskStatus.isChecked();
+                        listener.applyText(task,status);
+
+                    }
+                });
+
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (DialogListener) context;
+        } catch (ClassCastException e){
+            throw new ClassCastException(context.toString() + "must implement DialogListener");
+        }
+
+    }
+
+    public interface DialogListener{
+        void applyText(String task,Boolean status);
+    }
+}
