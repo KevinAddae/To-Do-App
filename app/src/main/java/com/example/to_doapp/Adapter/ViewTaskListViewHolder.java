c45440ccbb7aaebfc5f9739e7383f71ae7a1d5ce
KevinAddae <kevinaddae94@gmail.com>
2023-10-11 18:29:17 +0100

    Added delete image id to holder

--- app/src/main/java/com/example/to_doapp/Adapter/ViewTaskListViewHolder.java
+++ app/src/main/java/com/example/to_doapp/Adapter/ViewTaskListViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.to_doapp.Adapter;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,10 +13,12 @@ import com.example.to_doapp.R;
 public class ViewTaskListViewHolder extends RecyclerView.ViewHolder {
     TextView txtTask, txtStatus;
     RelativeLayout container;
+    ImageView imgDeleteTask;
     public ViewTaskListViewHolder(@NonNull View itemView) {
         super(itemView);
         txtTask = itemView.findViewById(R.id.txt_task);
         txtStatus = itemView.findViewById(R.id.txtStatus);
         container = itemView.findViewById(R.id.container);
+        imgDeleteTask = itemView.findViewById(R.id.img_delete);
     }
 }
